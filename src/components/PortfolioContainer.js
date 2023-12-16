d312a8756fc5f759d14af8ffbca422daaf960536
John Heger <heger7@gmail.com>
2023-12-15 19:57:04 -0600

    completed

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({ onPortfolioStocks, handleStockDelete }) {
+
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {onPortfolioStocks.map((info) => {
+        return (
+          <div>
+          <div className="card" onClick={() => handleStockDelete(info)} >
+            <div className="card-body">
+              <h5 className="card-title">{info.name}</h5>
+              <p className="card-text">{info.ticker}: {info.price}</p>
+            </div>
+          </div>
+        </div>
+      )
+      })}
     </div>
   );
 }
